b8ee23b31af6b7763f99ac82d7d33148dde5fd72
Husan <KhusanM>
2024-12-23 15:53:18 +0500

    Initial Commit

---
+++ qahvazor-client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qahvazor-client
+//
+//  Created by Alphazet on 23/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
