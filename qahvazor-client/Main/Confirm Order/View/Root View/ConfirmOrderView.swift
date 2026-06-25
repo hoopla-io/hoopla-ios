39a9512c6520f2305280578b5ac63fec76d87858
Khusan <khusanmasharipov1998@gmail.com>
2026-06-25 19:09:24 +0500

    addComment

--- qahvazor-client/Main/Confirm Order/View/Root View/ConfirmOrderView.swift
+++ qahvazor-client/Main/Confirm Order/View/Root View/ConfirmOrderView.swift
@@ -47,7 +47,11 @@ final class ConfirmOrderView: CustomView {
             syrupStackView.isHidden = true
         }
     }
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            configureTextView()
+        }
+    }
     @IBOutlet weak var priceButton: UIButton! {
         didSet {
             if #available(iOS 26.0, *) {
@@ -59,5 +63,45 @@ final class ConfirmOrderView: CustomView {
             }
         }
     }
+    
+    private enum TextViewLayout {
+        static let inset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    }
+
+    private let textViewPlaceholderLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "addComment".localized
+        label.textColor = .placeholderText
+        label.font = .systemFont(ofSize: 15)
+        label.isUserInteractionEnabled = false
+        return label
+    }()
 }
 
+extension ConfirmOrderView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        updateTextViewPlaceholderVisibility()
+    }
+}
+
+private extension ConfirmOrderView {
+    func configureTextView() {
+        textView.delegate = self
+        textView.textContainerInset = TextViewLayout.inset
+        textView.textContainer.lineFragmentPadding = 0
+        textView.addSubview(textViewPlaceholderLabel)
+
+        NSLayoutConstraint.activate([
+            textViewPlaceholderLabel.topAnchor.constraint(equalTo: textView.topAnchor, constant: TextViewLayout.inset.top),
+            textViewPlaceholderLabel.leadingAnchor.constraint(equalTo: textView.leadingAnchor, constant: TextViewLayout.inset.left),
+            textViewPlaceholderLabel.trailingAnchor.constraint(lessThanOrEqualTo: textView.trailingAnchor, constant: -TextViewLayout.inset.right)
+        ])
+
+        updateTextViewPlaceholderVisibility()
+    }
+
+    func updateTextViewPlaceholderVisibility() {
+        textViewPlaceholderLabel.isHidden = !textView.text.isEmpty
+    }
+}
